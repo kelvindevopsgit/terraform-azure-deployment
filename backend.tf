965374bf79dcbeaf3b3b80d57509161c26cacf68
kelvingit <kelvinenofedevops@gmail.com>
2024-11-15 13:00:51 +0000

    Deleted all files

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name   = "martinzexampleResourceGroup3"
-    storage_account_name  = "martinzresourcegroup3"
-    container_name        = "terraform-state"
-    key                   = "terraform.tfstate"
-  }
-}
--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "resource_group_name" {
-  value       = azurerm_resource_group.rg.name
-  description = "The name of the resource group"
-}
-
-output "sql_server_fqdn" {
-  value       = azurerm_mssql_server.sql_server.fully_qualified_domain_name
-  description = "The fully qualified domain name of the SQL Server"
-}
